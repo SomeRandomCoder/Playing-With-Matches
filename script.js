9d6d868f5b2943d8c4fbb7d81ffd5e5a75afee26
Tyron <tyron.burkhardt.za@gmail.com>
2016-02-24 17:46:36 +0200

    working on highLighting matching numbers

--- script.js
+++ script.js
@@ -3,37 +3,33 @@ var randomNumber = function(range){
   return Math.floor((Math.random() * 3)+1);
 };
 
-
+var num1 = document.getElementById("num1");
+var num2 = document.getElementById("num2");
+var num3 = document.getElementById("num3");
+var num4 = document.getElementById("num4");
+var num5 = document.getElementById("num5");
+var num6 = document.getElementById("num6");
+var num7 = document.getElementById("num7");
+var num8 = document.getElementById("num8");
+var num9 = document.getElementById("num9");
 
 divPrintR1 = function(){
-  var num1 = document.getElementById("num1");
-  num1.innerHTML=randomNumber();
 
-  var num2 = document.getElementById("num2");
+  num1.innerHTML = randomNumber();
   num2.innerHTML = randomNumber();
-
-  var num3 = document.getElementById("num3");
   num3.innerHTML = randomNumber();
 }
 
 divPrintR2 = function(){
-  var num4 = document.getElementById("num4");
+
   num4.innerHTML=randomNumber();
-
-  var num5 = document.getElementById("num5");
   num5.innerHTML = randomNumber();
-
-  var num6 = document.getElementById("num6");
   num6.innerHTML = randomNumber();
 }
 divPrintR3 = function(){
-  var num7 = document.getElementById("num7");
-  num7.innerHTML=randomNumber();
 
-  var num8 = document.getElementById("num8");
+  num7.innerHTML = randomNumber();
   num8.innerHTML = randomNumber();
-
-  var num9 = document.getElementById("num9");
   num9.innerHTML = randomNumber();
 }
 
