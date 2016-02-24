b5aeaa8f6c54c160608f00e8122bf5ad3f84e42f
Tyron <tyron.burkhardt.za@gmail.com>
2016-02-24 11:30:59 +0200

    Changes made to HTML and JS

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-<script type="text/javascript">
+
 var randomNumber = function(range){
   return Math.floor((Math.random() * 9)+1);
 };
-divPrint = function(){
-  var num = document.getElementById("num");
-  num.innerHTML=randomNumber();
+
+
+
+divPrintR1 = function(){
+  var num1 = document.getElementById("num1");
+  num1.innerHTML=randomNumber();
+
+  var num2 = document.getElementById("num2");
+  num2.innerHTML = randomNumber();
+
+  var num3 = document.getElementById("num3");
+  num3.innerHTML = randomNumber();
 }
+
+
 console.log (randomNumber());
-divPrint();
-  </script>
+divPrintR1();
