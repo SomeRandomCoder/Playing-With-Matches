8a847ced39e040a0a2b51140f6ae0273e4f3c7f7
Tyron <tyron.burkhardt.za@gmail.com>
2016-02-26 17:16:53 +0200

    highlight and display boxes work

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-var randomNumber = function(range){
+var randomNumber = function(){
   return Math.floor((Math.random() * 3)+1);
 };
 
@@ -14,74 +14,486 @@ var num8 = document.getElementById("num8");
 var num9 = document.getElementById("num9");
 var numbers = document.querySelector("number")
 
-divPrintR1 = function(){
+var row1=document.getElementById("row1");
+var row2=document.getElementById("row2");
+var row3=document.getElementById("row3");
+var col1=document.getElementsByClassName("col1");
+var col2=document.getElementsByClassName("col2");
+var col3=document.getElementsByClassName("col3");
 
+var numX= function(){
   num1.innerHTML = randomNumber();
   num2.innerHTML = randomNumber();
   num3.innerHTML = randomNumber();
-}
-
-divPrintR2 = function(){
-
-  num4.innerHTML=randomNumber();
+  num4.innerHTML = randomNumber();
   num5.innerHTML = randomNumber();
   num6.innerHTML = randomNumber();
-}
-divPrintR3 = function(){
-
   num7.innerHTML = randomNumber();
   num8.innerHTML = randomNumber();
   num9.innerHTML = randomNumber();
 }
 
 console.log (randomNumber());
-divPrintR1();
-divPrintR2();
-divPrintR3();
-// num1.classList.add('highLight');
-// num2.classList.add('highLight');
-// num3.classList.add('highLight');
-// num4.classList.add('highLight');
-// num5.classList.add('highLight');
-// num6.classList.add('highLight');
-// num7.classList.add('highLight');
-// num8.classList.add('highLight');
-// num9.classList.add('highLight');
+numX();
+
 
 function highLight(){
   var displayMessage = document.getElementById("displayMessage");
-if (num1.innerHTML === num2.innerHTML){
-num1.classList.add("highLight");
-num2.classList.add("highLight");
-displayMessage.innerHTML = "Number 1 and Number 2 match!";
+
+// if (num1.innerHTML == 1 )
+//   {
+//     num1.classList.add("highLight");
+//   }
+//
+// if (num1.innerHTML == 2)
+//   {
+//     num1.classList.add("highLight2");
+//   }
+//
+// if(num1.innerHTML == 3)
+//   {
+//     num1.classList.add("highLight3");
+//   }
+//
+// if (num2.innerHTML == 1 )
+//   {
+//     num2.classList.add("highLight");
+//   }
+//
+// if (num2.innerHTML == 2)
+//   {
+//     num2.classList.add("highLight2");
+//   }
+// if(num2.innerHTML == 3)
+//   {
+//     num2.classList.add("highLight3");
+//   }
+//
+// if (num3.innerHTML == 1 )
+//   {
+//     num3.classList.add("highLight");
+//   }
+// if (num3.innerHTML == 2)
+//   {
+//     num3.classList.add("highLight2");
+//   }
+// if(num3.innerHTML == 3)
+//   {
+//     num3.classList.add("highLight3");
+//   }
+//
+// if (num4.innerHTML == 1 )
+//   {
+//     num4.classList.add("highLight");
+//   }
+// if (num4.innerHTML == 2)
+//   {
+//     num4.classList.add("highLight2");
+//   }
+// if(num4.innerHTML == 3)
+//   {
+//     num4.classList.add("highLight3");
+//   }
+//
+// if (num5.innerHTML == 1 ){
+//   num5.classList.add("highLight");
+// }
+//  if (num5.innerHTML == 2){
+//   num5.classList.add("highLight2");
+// }
+//  if(num5.innerHTML == 3){
+//   num5.classList.add("highLight3");
+// }
+// if (num6.innerHTML == 1 ){
+//   num6.classList.add("highLight");
+// }
+//  if (num6.innerHTML == 2){
+//   num6.classList.add("highLight2");
+// }
+//  if(num6.innerHTML == 3){
+//   num6.classList.add("highLight3");
+// }
+// if (num7.innerHTML == 1 ){
+//   num7.classList.add("highLight");
+// }
+//  if (num7.innerHTML == 2){
+//   num7.classList.add("highLight2");
+// }
+//  if(num7.innerHTML == 3){
+//   num7.classList.add("highLight3");
+// }
+// if (num8.innerHTML == 1 ){
+//   num8.classList.add("highLight");
+// }
+//  if (num8.innerHTML == 2){
+//   num8.classList.add("highLight2");
+// }
+//  if(num8.innerHTML == 3){
+//   num8.classList.add("highLight3");
+// }
+// if (num9.innerHTML == 1 ){
+//   num9.classList.add("highLight");
+// }
+//  if (num9.innerHTML == 2){
+//   num9.classList.add("highLight2");
+// }
+//  if(num9.innerHTML == 3){
+//   num9.classList.add("highLight3");
+// }
+  var row1 =new function(){
+    if (num1.innerHTML == 1 )
+      {
+        num1.classList.add("highLight");
+      }
+    if (num1.innerHTML == 2)
+      {
+        num1.classList.add("highLight2");
+      }
+    if(num1.innerHTML == 3)
+      {
+        num1.classList.add("highLight3");
+      }
+    if (num2.innerHTML == 1 )
+      {
+        num2.classList.add("highLight");
+      }
+    if (num2.innerHTML == 2)
+      {
+        num2.classList.add("highLight2");
+      }
+    if(num2.innerHTML == 3)
+      {
+        num2.classList.add("highLight3");
+      }
+    if (num3.innerHTML == 1 )
+      {
+        num3.classList.add("highLight");
+      }
+    if (num3.innerHTML == 2)
+      {
+        num3.classList.add("highLight2");
+      }
+    if(num3.innerHTML == 3)
+      {
+        num3.classList.add("highLight3");
+      }
+  if(num1.innerHTML === num2.innerHTML){
+
+    displayMessageRow1.innerHTML = "Block 1 and Block 2 match! There are only two matching blocks.";
+  }
+ if (num1.innerHTML === num3.innerHTML){
+
+    displayMessageRow1.innerHTML ="Block 1 and Block 3 match! There are only two matching blocks.";
+  }
+    if(num2.innerHTML === num3.innerHTML){
+
+    displayMessageRow1.innerHTML = "Block 2 and Block 3 match! There are only two matching blocks.";
+  }
+    if(num1.innerHTML === num2.innerHTML && num2.innerHTML === num3.innerHTML && num1.innerHTML === num3.innerHTML){
+
+     displayMessageRow1.innerHTML = "Row 1 Matches";
+  }
+
+  if(num1.innerHTML != num2.innerHTML && num2.innerHTML != num3.innerHTML && num1.innerHTML != num3.innerHTML){
+
+   displayMessageRow1.innerHTML = "There are no Matches";
 }
 
-if(num1.innerHTML === num3.innerHTML){
-  num1.classList.add("highLight");
-  num3.classList.add("highLight");
-  displayMessage.innerHTML ="Number 1 and Number 3 match!";
 }
 
-if(num2.innerHTML === num3.innerHTML){
-  num2.classList.add("highLight");
-  num3.classList.add("highLight");
-  displayMessage.innerHTML = "Number 2 and Number 3 match!";
+  var row2 =new function(){
+    if (num4.innerHTML == 1 )
+      {
+        num4.classList.add("highLight");
+      }
+
+  else if (num4.innerHTML == 2)
+      {
+        num4.classList.add("highLight2");
+      }
+
+  else if(num4.innerHTML == 3)
+      {
+        num4.classList.add("highLight3");
+      }
+
+    if (num5.innerHTML == 1 )
+      {
+        num5.classList.add("highLight");
+      }
+
+    else if (num5.innerHTML == 2)
+      {
+        num5.classList.add("highLight2");
+      }
+    else if(num5.innerHTML == 3)
+      {
+        num5.classList.add("highLight3");
+      }
+
+    if (num6.innerHTML == 1 )
+      {
+        num6.classList.add("highLight");
+      }
+    else if (num6.innerHTML == 2)
+      {
+        num6.classList.add("highLight2");
+      }
+    else if(num6.innerHTML == 3)
+      {
+        num6.classList.add("highLight3");
+      }
+
+
+        if(num4.innerHTML === num5.innerHTML)
+           {
+             displayMessageRow2.innerHTML = "Block 4 and Block 5 match! There are only two matching blocks.";
+           }
+       if (num4.innerHTML === num6.innerHTML)
+           {
+             displayMessageRow2.innerHTML ="Block 4 and Block 6 match! There are only two matching blocks.";
+           }
+          if(num5.innerHTML === num6.innerHTML)
+           {
+             displayMessageRow2.innerHTML = "Block 5 and Block 6 match! There are only two matching blocks.";
+           }
+          if(num4.innerHTML === num5.innerHTML && num4.innerHTML === num6.innerHTML && num5.innerHTML === num6.innerHTML)
+           {
+             displayMessageRow2.innerHTML = "Row 2 matches!";
+           }
+           if(num4.innerHTML != num5.innerHTML && num4.innerHTML != num6.innerHTML && num5.innerHTML != num6.innerHTML)
+            {
+              displayMessageRow2.innerHTML = "Row 2 matches!";
+            }
+
 }
-if (num1.innerHTML===num2.innerHTML && num1.innerHTML===num3.innerHTML && num2.innerHTML===num3.innerHTML){
-  num1.classList.add("highLight");
-  num2.classList.add("highLight");
-  num3.classList.add("highLight");
-  displayMessage.innerHTML = "Number 1, Number 2 and Number 3 matches!";
+      var row3 =new function(){
+        if (num7.innerHTML == 1 )
+          {
+            num7.classList.add("highLight");
+          }
+
+       if (num7.innerHTML == 2)
+          {
+            num7.classList.add("highLight2");
+          }
+
+       if(num7.innerHTML == 3)
+          {
+            num7.classList.add("highLight3");
+          }
+
+        if (num8.innerHTML == 1 )
+          {
+            num8.classList.add("highLight");
+          }
+
+       if (num8.innerHTML == 2)
+          {
+            num8.classList.add("highLight2");
+          }
+        if(num8.innerHTML == 3)
+          {
+            num8.classList.add("highLight3");
+          }
+
+        if (num9.innerHTML == 1 )
+          {
+            num9.classList.add("highLight");
+          }
+         if (num9.innerHTML == 2)
+          {
+            num9.classList.add("highLight2");
+          }
+         if(num9.innerHTML == 3)
+          {
+            num9.classList.add("highLight3");
+          }
+
+
+  if(num7.innerHTML === num8.innerHTML)
+     {
+       displayMessageRow3.innerHTML = "Block 7 and Block 8 match! There are only two matching blocks.";
+     }
+    if (num7.innerHTML === num9.innerHTML)
+     {
+       displayMessageRow3.innerHTML ="Block 7 and Block 9 match! There are only two matching blocks.";
+     }
+    if(num8.innerHTML === num9.innerHTML)
+     {
+       displayMessageRow3.innerHTML = "Block 8 and Block 9 match! There are only two matching blocks.";
+     }
+    if(num7.innerHTML === num8.innerHTML && num8.innerHTML === num9.innerHTML && num7.innerHTML === num9.innerHTML)
+     {
+       displayMessageRow3.innerHTML = "Row 3 matches!";
+     }
+     if(num7.innerHTML != num8.innerHTML && num8.innerHTML != num9.innerHTML && num7.innerHTML != num9.innerHTML)
+      {
+        displayMessageRow3.innerHTML = "There are no matches!";
+      }
+
 }
-if(num1.innerHTML === num4.innerHTML){
-  num1.classList.add("highLight");
-  num4.classList.add("highLight");
-  displayMessage.innerHTML ="Number 1 and Number 4 matches!";
+ var col1 =new function(){
+   if (num1.innerHTML == 1 )
+     {
+       num1.classList.add("highLight");
+     }
+
+   if (num1.innerHTML == 2)
+     {
+       num1.classList.add("highLight2");
+     }
+
+   if(num1.innerHTML == 3)
+     {
+       num1.classList.add("highLight3");
+     }
+   if (num4.innerHTML == 1 )
+     {
+       num4.classList.add("highLight");
+     }
+   if (num4.innerHTML == 2)
+     {
+       num4.classList.add("highLight2");
+     }
+   if(num4.innerHTML == 3)
+     {
+       num4.classList.add("highLight3");
+     }
+   if (num7.innerHTML == 1 ){
+     num7.classList.add("highLight");
+   }
+    if (num7.innerHTML == 2){
+     num7.classList.add("highLight2");
+   }
+    if(num7.innerHTML == 3){
+     num7.classList.add("highLight3");
+   }
+
+
+ if(num1.innerHTML === num4.innerHTML)
+   {
+    displayMessageCol1.innerHTML = "Block 1 and Block 4 match!There are two matches!";
+   }
+ if (num1.innerHTML === num7.innerHTML)
+   {
+    displayMessageCol1.innerHTML ="Block 1 and Block 7 match!There are two matches!";
+   }
+  if(num4.innerHTML === num7.innerHTML)
+ {
+   displayMessageCol1.innerHTML = "Block 4 and Block 7 match!There are two matches!";
+ }
+ if(num1.innerHTML === num4.innerHTML && num4.innerHTML === num7.innerHTML && num1.innerHTML === num7.innerHTML)
+ {
+  displayMessageCol1.innerHTML = "Column 1 matches!";
+ }
+ if(num1.innerHTML != num4.innerHTML && num4.innerHTML != num7.innerHTML && num1.innerHTML != num7.innerHTML){
+   displayMessageCol1.innerHTML ="There are no matches!";
+ }
+
 }
+
+ var col2 = new function(){
+
+ if(num2.innerHTML === num5.innerHTML)
+   {
+      displayMessageCol2.innerHTML="Block2 and Block 5 match!THere are two matches!";
+   }
+ if (num2.innerHTML === num8.innerHTML)
+   {
+      displayMessageCol2.innerHTML="Block2 and Block 8 match!THere are two matches!";
+   }
+ if(num5.innerHTML === num8.innerHTML)
+   {
+    displayMessageCol2.innerHTML="Block 5 and Block 8 match!THere are two matches!";
+   }
+ if(num2.innerHTML === num5.innerHTML && num5.innerHTML === num8.innerHTML && num2.innerHTML === num8.innerHTML)
+   {
+    displayMessageCol2.innerHTML="Column 2 matches!";
+   }
+   if(num2.innerHTML != num5.innerHTML && num5.innerHTML != num8.innerHTML && num2.innerHTML != num8.innerHTML)
+     {
+      displayMessageCol2.innerHTML="There are no matches!";
+     }
+
+ }
+
+var col3 = new function(){
+  if (num3.innerHTML == 1 )
+    {
+      num3.classList.add("highLight");
+    }
+  if (num3.innerHTML == 2)
+    {
+      num3.classList.add("highLight2");
+    }
+  if(num3.innerHTML == 3)
+    {
+      num3.classList.add("highLight3");
+    }
+  if (num6.innerHTML == 1 ){
+    num6.classList.add("highLight");
+  }
+   if (num6.innerHTML == 2){
+    num6.classList.add("highLight2");
+  }
+   if(num6.innerHTML == 3){
+    num6.classList.add("highLight3");
+  }
+   if (num9.innerHTML == 1 ){
+    num9.classList.add("highLight");
+  }
+   if (num9.innerHTML == 2){
+    num9.classList.add("highLight2");
+  }
+   if(num9.innerHTML == 3){
+    num9.classList.add("highLight3");
+  }
+  if(num3.innerHTML === num6.innerHTML){
+    displayMessageCol3.innerHTML = "Block 3 and Block 6 match! There are only two matches!";
+  }
+  if (num3.innerHTML === num9.innerHTML){
+      displayMessageCol3.innerHTML = "Block 3 and Block 9 match!";
+  }
+  if(num6.innerHTML === num9.innerHTML){
+    displayMessageCol3.innerHTML = "Block 6 and Block 9 match!";
+  }
+  if(num3.innerHTML === num6.innerHTML && num6.innerHTML === num9.innerHTML && num3.innerHTML === num9.innerHTML){
+    displayMessageCol3.innerHTML ="Column 3 matches!";
+  }
+  if(num3.innerHTML != num6.innerHTML && num3.innerHTML != num9.innerHTML && num6.innerHTML != num9.innerHTML){
+    displayMessageCol3.innerHTML="There are no matches!"
+  }
+
+
+}
+
+
+
+
+
+
+
+
 
 };
-
-
-
 highLight();
 displayMessage();
+row1();
+row2();
+ row3();
+ col1();
+ col2();
+ col3();
+
+
+
+
+
+// function button(){
+//   var button = document.getElementById("Refresh");
+//     remove.document.classList.remove("highLight");
+//     divPrintR1();
+//     highLight();
+//
+// };
